76e0621b9dc4207dd2ab18a07fbd45228cfab0df
Michael Raysor <michaelraysor1@gmail.com>
2021-03-02 23:18:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+def dsiplay_board
+  
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
+end
